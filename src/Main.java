e6c645c7c3a5a22aba8a303ba940ac3b474708ff
Victor Alves <victor_xd97@yahoo.com.br>
2018-06-19 16:00:15 -0300

    incrementando as telas de navegacao

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,40 @@
+import controler.AutoresController;
+import controler.EditorasController;
 import controler.GenerosController;
+import controler.UsuariosController;
+import view.NavegacaoView;
 
 public class Main {
 
 	public static void main(String[] args) {
 		GenerosController gcontroller = new GenerosController();
-		gcontroller.CriaGenero();
-
+		UsuariosController ucontroller = new UsuariosController();
+		EditorasController econtroller = new EditorasController();
+		AutoresController acontroller = new AutoresController();
+		
+		//TELA
+		NavegacaoView menu = new NavegacaoView();
+		menu.telaInicial();
+		
+		//gcontroller.Criar();
+		//gcontroller.Consultar();
+		//gcontroller.Deletar();
+		//gcontroller.Alterar();
+		
+		//ucontroller.Criar();
+		//ucontroller.Consultar();
+		//ucontroller.Deletar();
+		//ucontroller.Alterar();
+		
+		//econtroller.Criar();
+		//econtroller.Consultar();
+		//econtroller.Deletar();
+		//econtroller.Alterar();
+		
+		//acontroller.Criar();
+		//acontroller.Consultar();
+		//acontroller.Deletar();
+		//acontroller.Alterar();
 	}
 
 }
